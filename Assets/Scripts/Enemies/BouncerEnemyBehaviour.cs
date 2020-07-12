cafacc15b0aa2d5b53b4fe24275fe816ae5135e8
Owen Shevlin <owen2284@gmail.com>
2020-07-12 12:06:31 +0100

    Added Bouncer enemy

---
+++ Assets/Scripts/Enemies/BouncerEnemyBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncerEnemyBehaviour : BaseEnemyBehaviour
+{
+    protected override void Setup() {
+        secondsToNextCannon = cannonCooldownTime + (Random.Range(0, 0.50f) * cannonCooldownTime);
+    }
+
+    protected override void HandleFiring() {
+        if (targetPlayer != null) {
+            if (secondsToNextCannon > 0) {
+                secondsToNextCannon -= Time.deltaTime;
+            }
+
+            if (secondsToNextCannon <= 0) {
+                CannonBehaviour currentCannon = cannons[nextCannon];
+                currentCannon.FireCannon(true);
+
+                secondsToNextCannon = cannonCooldownTime + (Random.Range(0, 0.50f) * cannonCooldownTime);
+                nextCannon += 1;
+                if (nextCannon >= cannons.Count) nextCannon = 0;
+            }
+        }
+    }
+
+    protected override void HandleCollision(GameObject target) {
+        base.HandleCollision(target);
+
+        if (target.tag == "Boundary") {
+            moveVector = new Vector2(moveVector.x, moveVector.y * -1);
+        }
+    }
+}
--- Assets/Scripts/Player/CannonBehaviour.cs
+++ Assets/Scripts/Player/CannonBehaviour.cs
@@ -6,15 +6,34 @@ public class CannonBehaviour : MonoBehaviour
 {
     public GameObject projectile;
     public float fireSpeed;
+    public bool facePlayer;
+
+    private float originalRotation;
+    private GameObject targetPlayer;
 
     void Start() {
-        
+        originalRotation = transform.rotation.z;
+        targetPlayer = GameObject.FindWithTag("Player");
     }
 
-    public void FireCannon() {
+    void Update() {
+        if (facePlayer && targetPlayer != null) {
+            transform.up = targetPlayer.transform.position - transform.position;
+        }
+        else {
+            //transform.eulerAngles = new Vector3(0, 0, originalRotation);
+        }
+    }
+
+    public void FireCannon(bool backwards = false) {
         GameObject spawnedProjectile = (GameObject)Instantiate(projectile, transform.position, transform.rotation);
 
-        Vector2 fireVelocity = new Vector2(fireSpeed, fireSpeed) * transform.up;
+        float trueFireSpeed = fireSpeed;
+        if (backwards) {
+            trueFireSpeed = trueFireSpeed * -1;
+        }
+
+        Vector2 fireVelocity = new Vector2(trueFireSpeed, trueFireSpeed) * transform.up;
         spawnedProjectile.GetComponent<BaseProjectileBehaviour>().velocity = fireVelocity;
     }
 }
